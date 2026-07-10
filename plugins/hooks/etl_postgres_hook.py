9622e0766a106316f90db6ec3db4d41fd5fa26f3
neetikaneupane <neetikaneupane@gmail.com>
2026-07-10 12:44:00 +0545

    fix: use psycopg2.sql for safe identifier quoting in EtlPostgresHook

--- plugins/hooks/etl_postgres_hook.py
+++ plugins/hooks/etl_postgres_hook.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from psycopg2 import sql
 from airflow.hooks.base import BaseHook
 
 log = logging.getLogger(__name__)
@@ -18,20 +19,37 @@ class EtlPostgresHook(BaseHook):
     def get_conn(self):
         if self._conn is None or self._conn.closed:
             conn_details = BaseHook.get_connection(self.conn_id)
-            self._conn = psycopg2.connect(
-                host=conn_details.host,
-                port=conn_details.port or 5432,
-                dbname=conn_details.schema,
-                user=conn_details.login,
-                password=conn_details.password,
-                connect_timeout=10,
-            )
+            extra = conn_details.extra_dejson if conn_details.extra else {}
+            sslmode = extra.get("sslmode", "prefer")
+            sslrootcert = extra.get("sslrootcert")
+            sslcert = extra.get("sslcert")
+            sslkey = extra.get("sslkey")
+            connect_kwargs = {
+                "host": conn_details.host,
+                "port": conn_details.port or 5432,
+                "dbname": conn_details.schema,
+                "user": conn_details.login,
+                "password": conn_details.password,
+                "connect_timeout": 10,
+                "sslmode": sslmode,
+            }
+            if sslrootcert:
+                connect_kwargs["sslrootcert"] = sslrootcert
+            if sslcert:
+                connect_kwargs["sslcert"] = sslcert
+            if sslkey:
+                connect_kwargs["sslkey"] = sslkey
+            self._conn = psycopg2.connect(**connect_kwargs)
         return self._conn
 
     def extract_data(self, schema: str, table: str, chunk_size: int = 10000):
         conn = self.get_conn()
+        query = sql.SQL("SELECT * FROM {schema}.{table}").format(
+            schema=sql.Identifier(schema),
+            table=sql.Identifier(table),
+        )
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute(f'SELECT * FROM {schema}.{table}')
+            cur.execute(query)
             columns = [desc[0] for desc in cur.description]
             while True:
                 rows = cur.fetchmany(chunk_size)
@@ -42,8 +60,12 @@ class EtlPostgresHook(BaseHook):
 
     def get_row_count(self, schema: str, table: str) -> int:
         conn = self.get_conn()
+        query = sql.SQL("SELECT COUNT(*) FROM {schema}.{table}").format(
+            schema=sql.Identifier(schema),
+            table=sql.Identifier(table),
+        )
         with conn.cursor() as cur:
-            cur.execute(f'SELECT COUNT(*) FROM {schema}.{table}')
+            cur.execute(query)
             return cur.fetchone()[0]
 
     def close(self):
