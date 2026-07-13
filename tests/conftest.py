579ff416a2eb9f9d63e0e1def0225eddf9ebbc79
neetikaneupane <neetikaneupane@gmail.com>
2026-07-13 22:07:28 +0545

    test: add airflow test isolation via _AIRFLOW__AS_LIBRARY env var

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import os
 import sys
 
+# Prevent Airflow from initializing during test collection
+os.environ["_AIRFLOW__AS_LIBRARY"] = "1"
+os.environ["AIRFLOW_HOME"] = "/tmp/airflow"
+os.environ["AIRFLOW__CORE__UNIT_TEST_MODE"] = "True"
+os.environ["AIRFLOW__CORE__LOAD_EXAMPLES"] = "False"
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "plugins"))
